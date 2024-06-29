eb48a40652270af730c1c659882f44c11af80577
Paulo <pvpmartins@hotmail.com>
2024-06-29 16:37:55 -0300

    wip

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -20,6 +20,7 @@ resource "aws_subnet" "public" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "sa-east-1a"
+  map_public_ip_on_launch = true
   tags = {
     Name = "PublicSubnet"
   }
