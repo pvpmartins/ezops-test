6cf5b0ede88d4ef3612302649e3f9229b192fe2e
Paulo <pvpmartins@hotmail.com>
2024-06-28 22:49:36 -0300

    wip

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,25 +2,25 @@ provider "aws" {
   region = "sa-east-1"
 }
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "test-paulovitor-state"
-  acl    = "private"
-
-  versioning {
-    enabled = true
-  }
-}
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
+#resource "aws_s3_bucket" "terraform_state" {
+#  bucket = "test-paulovitor-state"
+#  acl    = "private"
+#
+#  versioning {
+#    enabled = true
+#  }
+#}
+#
+#resource "aws_dynamodb_table" "terraform_locks" {
+#  name         = "teste-paulo-vitor-lock-state-table"
+#  billing_mode = "PAY_PER_REQUEST"
+#  hash_key     = "LockID"
+#
+#  attribute {
+#    name = "LockID"
+#    type = "S"
+#  }
+#}
 
 terraform {
   backend "s3" {
