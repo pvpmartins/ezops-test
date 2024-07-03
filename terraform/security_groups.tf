e17b54bef646de414114cbfeaaafb883373cdafc
Paulo <pvpmartins@hotmail.com>
2024-07-03 18:28:08 -0300

    wip

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -33,6 +33,14 @@ resource "aws_security_group" "allow_all" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Expose port to external access"
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     description = "Allow all outbound traffic"
     from_port   = 0
