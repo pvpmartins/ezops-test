b6a6b7af89912ef5f02cf4083c6c99092c4c9296
Paulo <pvpmartins@hotmail.com>
2024-07-03 22:55:52 -0300

    wip

--- apps/frontend/src/.env.js
+++ apps/frontend/src/.env.js
@@ -1,4 +1,4 @@
-export const API_PORT = '5000'
-export const API_URL = `http://localhost:${API_PORT}`
+export const API_PORT = '31177'
+export const API_URL = `http://54.207.154.251:${API_PORT}`
 export const DOMAIN_TITLE = 'supersite.com'
 
